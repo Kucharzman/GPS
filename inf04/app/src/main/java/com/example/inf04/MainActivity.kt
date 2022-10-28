d4ff2f2e34a57c98129de79ef37c5aa6a145afc4
test <test@asd.pl>
2022-10-28 09:09:07 +0200

    stary, najpierw pushujesz commita, potem się wylogowywujesz ze WSZYSTKIEGO na tym kompie
    by wylogować się z Gita na Android Studio należy:
    Android Studio(na górnym pasku)>Preferences>Version Control>Github>Usuwasz Konto z listy
    
    pozdro z Malibu

--- inf04/app/src/main/java/com/example/inf04/MainActivity.kt
+++ inf04/app/src/main/java/com/example/inf04/MainActivity.kt
@@ -2,10 +2,33 @@ package com.example.inf04
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        /************************************************************************************
+         * nazwa funkcji:       bttConfirmOnClick
+         * parametry wejściowe:
+         * wartość zwracacane:  tekst informujący użytkownika o logowaniu zwracany do labela
+         * autor:               Dejwa Kubanek
+         */
+        findViewById<Button>(R.id.bttConfirm).setOnClickListener {
+
+            val sMail = findViewById<EditText>(R.id.txtMail).text.toString()
+            val sPass = findViewById<EditText>(R.id.txtPass).text.toString()
+            val sPassRep = findViewById<EditText>(R.id.txtPassRep).text.toString()
+
+            if (sPass == sPassRep){
+                findViewById<TextView>(R.id.lblOutput).text = "Witaj " + sMail
+            }else{
+                findViewById<TextView>(R.id.lblOutput).text = "Hasła się różnią"
+            }
+
+        }
     }
 }
